e0113c796b38bdbfde201b5c18e1c11823d7b7da
dashluu <dashluu9121997@gmail.com>
2024-07-07 13:11:22 -0700

    Refined text summarization.

--- tasks/nlp.py
+++ tasks/nlp.py
@@ -1,23 +1,18 @@
 from transformers import pipeline
+from llama_index.llms.ollama import Ollama
 
-summary_model = "facebook/bart-large-cnn"
+llm = Ollama(model="llama3", request_timeout=60.0)
+
 qa_model = "deepset/roberta-base-squad2"
-summarizer = pipeline("summarization", model=summary_model, device="mps")
 qa = pipeline("question-answering", model=qa_model, tokenizer=qa_model, device="mps")
 
 
 def summarize(text):
-    tokens = summarizer.tokenizer.encode(text)
-    max_len = int(len(tokens) * 0.7)
-    min_len = int(len(tokens) * 0.2)
-    result = summarizer(text, max_length=max_len, min_length=min_len, do_sample=False)
-    return result[0]["summary_text"]
+    result = llm.complete(f"Summarize the following text:\n{text}")
+    return result.text
 
 
 def answer_question(question, context):
-    qa_input = {
-        "question": question,
-        "context": context
-    }
+    qa_input = {"question": question, "context": context}
     result = qa(qa_input)
     return result
